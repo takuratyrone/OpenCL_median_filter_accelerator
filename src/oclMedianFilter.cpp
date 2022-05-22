d17fc72a5455f418578e7d853c73ab4ad281d1e2
takuratyrone <ttkawome@gmail.com>
2022-05-22 22:07:41 +0200

    median filtered output

--- src/oclMedianFilter.cpp
+++ src/oclMedianFilter.cpp
@@ -56,7 +56,7 @@ int main(void)
 	unsigned int resHeight;
 	unsigned int maxValue;
 
-	ifstream infile("balloons_noisy.ascii.pgm");
+	ifstream infile("glassware_noisy.ascii.pgm");
 	stringstream ss;
 	string inputLine = "";
 
